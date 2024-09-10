8d53d6be21593fd5af4f6a4a233fb0d217fe02a1
Marcade-B <benoitmarcade3@gmail.com>
2024-09-10 10:56:47 +0200

    scrabble-update

--- cours-1/scrabble/scrabble.py
+++ cours-1/scrabble/scrabble.py
@@ -1,19 +1,45 @@
-dic = {"a":1, "b":3, "c":3, "d":2, "e":1, "f":4, "g":2, "u":1}
 
-def scrabble (mot, dic) :
+def scrabble (mot) :
+    dic = {"a":1, "b":3, "c":3, "d":2, "e":1, "f":4, "g":2, "u":1}
     result = 0
     for i in mot :
         if (i in dic) :
             result = dic[i] + result
     return result
 
-print(scrabble("cafe", dic))
+# print(scrabble("cafe"))
 
-tab = ["def", "atk", "hp", "res"]
-dico = {tab[0]:0}
-dico[tab[1]] = 1
-print(dico)
+# tab = ["def", "atk", "hp", "res"]
+# dico = {tab[0]:0}
+# dico[tab[1]] = 1
+# print(dico[1])
 
+mot = ["cafe", "face", "button", "agregre"]
 
 def trie (mot) :
-    score = {}
+    reste_val = 0
+    reste_mot = ""
+    k = 0
+    dico_valeur = []
+    if (len(mot) == 0) :
+        return "Il n'y a pas de mot dans la liste"
+    if (len(mot) == 1) :
+        return (mot[0])
+    for i in mot :
+        dico_valeur.append(scrabble(i))
+        k += 1
+    for i in range (len(dico_valeur) - 1) :
+        if dico_valeur[i] < dico_valeur[i+1] :
+            reste_val = dico_valeur[i]
+            reste_mot = mot[i]
+            dico_valeur[i] = dico_valeur[i+1]
+            mot[i] = mot[i+1]
+            dico_valeur[i+1] = reste_val
+            mot[i+1] = reste_mot
+    for i in range (len(dico_valeur)) :
+        print (mot[i])
+    return ""
+    
+    
+
+print(trie(mot))
