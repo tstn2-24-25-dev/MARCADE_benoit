2d92197a05e745e35c761fe4576c9da517783ff7
Marcade-B <benoitmarcade3@gmail.com>
2024-09-13 15:45:14 +0200

    inventaire-update

--- cours-1/inventaire/inventaire.py
+++ cours-1/inventaire/inventaire.py
@@ -1,29 +1,75 @@
-import random
-dic = {"jus" : 15, "pq" : 3, "coca" : 5, "cayou" : 77, "mayo" : 14, "chips" : 6, "telephone" : 17, "vache" : 29, "acer predator" : 61, "lait" : 128}
-produit = ["jus", "pq", "coca", "cayou", "mayo", "chips", "telephone", "vache", "acer predator", "lait"]
+# Ouverture du fichier txt
+art = open("inventaire.txt", "r")
 
-def article (produit) :
-    # prix = []
-    liste_produit = []
-    nb_article = random.randint(10,30)
-    for i in range (nb_article+1) :
-        random_produit = random.randint(0,len(produit)-1)
-        liste_produit.append(produit[random_produit])
-        # prix.append(dic[produit[random_produit]])
-    return liste_produit #, prix
+# Récupération de la première ligne du fichier txt correspondant au nombres d'articles
+nb_art = art.readline().strip()
 
-def calcul(produit, dic) :
-    resultat = []
+
+
+def article () :
+
+    # Récupération du reste des lignes du fichier txt correspondant aux nombres d'articles 
+    liste_art = art.readlines()
+    
+    for i in range (len(liste_art)) :
+    
+        # Retire les \n dans la liste
+        liste_art[0] = liste_art[0].strip()
+
+        # Sépare les articles et leurs prix
+        liste_art.append(liste_art[0].split())
+
+        liste_art.pop(0)
+    
+    return (liste_art)
+
+
+
+
+def calcul() :
+
+    # Liste permettant de vérifié si l'article n'a pas déjà était traité
+    already = []
+    
     prix = {}
-    articles = article(produit)
+    
+    articles = article()
+    
     for i in articles :
-        if not (i in resultat) :
-            resultat.append(i)
-            prix[i] = dic[i]
+        
+        # Si l'article n'a pas encore été traiter
+        if not (i[0] in already) :
+            
+            # Ajout de l'article dans la liste
+            already.append(i[0])
+    
+            # Ajout de la valeur de l'article ainsi que son nom dans un dictionnaire
+            prix[i[0]] = (int(i[1]))
+    
+        # Si l'article n'a pas encore été traiter
         else :
-            prix[i] *= 2
+
+            for k in prix :
+            
+                if k == i[0] :
+                    
+                    # Mise à jour du prix de l'article présent en plusieurs exemplaire
+                    prix[k] += int(i[1])
+    
+
+    """
+    A modifier pour traiter le cas où 
+    2 articles ont la même valeur finale
+    """
     max_prix = max(prix.values())
     
-    return max_prix
+    # Recherche de l'indice de la valeur la plus grande
+    for i in prix :
+    
+        if prix[i] == max_prix :
 
-print (calcul(produit, dic))
+            return f"{i} {prix[i]}"
+
+
+# Appel du programme
+print(calcul())
